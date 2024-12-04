87d9384e168533ca9daa413c24d57ec80bebab0c
christianastudillo <you@example.com>
2024-12-04 12:29:48 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import materia.Stacks.Stack;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        runStack();
+    }
+    public static void runStack() {
+        Stack stack = new Stack();
+        stack.push(5);
+        stack.push(7);
+        stack.push(10);
+        stack.push(20);
+        System.out.println("Cima --> " + stack.peek());
+        System.out.println("Retirar --> " + stack.pop());
+        System.out.println("Cima --> " + stack.peek());
+        System.out.println("Retirar --> " + stack.pop());
+        System.out.println("Cima --> " + stack.peek());
+    }
+}
---
+++ src/materia/Stacks/Stack.java
@@ -0,0 +1,42 @@
+package materia.Stacks;
+
+import java.util.EmptyStackException;
+
+import materia.models.Node;
+
+public class Stack {
+    
+    private Node top;//nodo en la cima de nustra pila
+
+    //creamos la pila con la cima nulla o vacia 
+    public Stack(){
+        this.top = null;
+    }
+    public void push (int value){
+        Node newNode = new Node(value);
+        newNode.setNext(top);
+        top = newNode;
+    }
+
+    public int pop(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        int value = top.getValue();
+        top = top.getNext();
+        return value;
+    }
+
+    public int peek(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        return top.getValue();
+    }
+
+    public boolean isEmpty(){
+        return top == null;
+    }
+
+
+}
